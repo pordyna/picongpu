eb9351bf5adf29a5b13f760a1888baf00d1ff5c3
Paweł Ordyna <p.ordyna@hzdr.de>
2025-10-07 16:20:47 +0200

    make the logging about not loading rng states critical

--- include/picongpu/plugins/openPMD/openPMDWriter.x.cpp
+++ include/picongpu/plugins/openPMD/openPMDWriter.x.cpp
@@ -1357,7 +1357,7 @@ make sure that environment variable OPENPMD_BP_BACKEND is not set to ADIOS1.
                 }
                 else
                 {
-                    log<picLog::INPUT_OUTPUT>("openPMD: Skiping loading RNG states on request!");
+                    log<picLog::CRITICAL>("openPMD: Skiping loading RNG states on request!");
                 }
 #    endif
 
