81fb7701996908001adfb91b2cb5b4b5fbf536bc
pordyna <p.ordyna@hzdr.de>
2025-11-10 11:56:17 -0800

    fix DensityWeighted start position

--- include/picongpu/particles/startPosition/functors.hpp
+++ include/picongpu/particles/startPosition/functors.hpp
@@ -20,11 +20,11 @@
 
 #pragma once
 
+#include "picongpu/particles/startPosition/DensityWeightedPPCRandomPositionImpl.hpp"
 #include "picongpu/particles/startPosition/OnePositionImpl.hpp"
 #include "picongpu/particles/startPosition/QuietImpl.hpp"
 #include "picongpu/particles/startPosition/RandomBinomialImpl.hpp"
 #include "picongpu/particles/startPosition/RandomImpl.hpp"
 #include "picongpu/particles/startPosition/RandomPositionAndWeightingImpl.hpp"
-#include "picongpu/particles/startPosition/generic/DensityWeightedPPCRandomPositionImpl.def"
 #include "picongpu/particles/startPosition/generic/Free.hpp"
 #include "picongpu/particles/startPosition/generic/FreeRng.hpp"
