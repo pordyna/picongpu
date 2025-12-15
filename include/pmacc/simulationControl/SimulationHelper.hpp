ee92615d5729048679140f9855096a33b503857a
pordyna <p.ordyna@hzdr.de>
2025-12-15 02:09:42 -0800

    Revert "vibe coded fix for simulation freezing on signaling"
    This reverts commit 2cde5ded4c75f6229a2a92afbed2c494a08cc6e2.

--- include/pmacc/simulationControl/SimulationHelper.hpp
+++ include/pmacc/simulationControl/SimulationHelper.hpp
@@ -166,11 +166,6 @@ namespace pmacc
         MPI_Request signalMPI = MPI_REQUEST_NULL;
         bool signalCreateCheckpoint = false;
         bool signalStopSimulation = false;
-        /** Flag to track if a signal is currently being processed */
-        bool processingSignal = false;
-        /** Accumulate multiple signals during processing */
-        bool pendingCreateCheckpoint = false;
-        bool pendingStopSimulation = false;
 
         void checkSignals(uint32_t const currentStep);
 
