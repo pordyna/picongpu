b03b4531758b4206db94a007ea31a32e82dfe6ff
pordyna <p.ordyna@hzdr.de>
2025-12-19 00:09:10 -0800

    add missing wait for events

--- include/pmacc/simulationControl/SimulationHelper.cpp
+++ include/pmacc/simulationControl/SimulationHelper.cpp
@@ -202,6 +202,7 @@ namespace pmacc
                 dumpOneStep(currentStep);
                 // synchronize steps: this is a temporary fix for a potential signaling error
                 // TODO: Discuss if this is the wy to fix the issue, if so use persistant barrier instead.
+                eventSystem::waitForAllTasks();
                 MPI_CHECK(MPI_Barrier(Environment<DIM>::get().GridController().getCommunicator()..getMPIComm()));
             }
 
